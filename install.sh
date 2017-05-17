0d9c21d37f1b74a220a00526a818fae74751322d
Quentin Comte-Gaz <quentin@comte-gaz.com>
2017-05-17 16:09:46 +0200

    install: Improve the install

--- install.sh
+++ install.sh
@@ -1,6 +1,9 @@
-#!/bin/bash
-# Use only if you need to perform changes on the user system such as installing software
-sudo apt-get install -y xmms2
+#!/usr/bin/env bash
+
+# Install the music player
+[[ -z $(which xmms2) ]] && sudo apt-get --yes --force-yes install xmms2
+
+# Prepare the player
 xmms2 playlist remove music
 xmms2 playlist create music
 xmms2 playlist switch music
